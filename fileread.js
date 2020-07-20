76eb2dea34aeea32c50623cfdd338509522cb55e
Sufyan Abbada <62093556+SufyanAbbada@users.noreply.github.com>
2020-07-20 23:31:22 +0500

    Update fileread.js

--- fileread.js
+++ fileread.js
@@ -7,3 +7,5 @@ let using = fs.readFile("sometext.txt", "utf8", (error, data) => {
   console.log(data);
 });
 console.log("Data Read!");
+//This last Line is executed before the upper printing because we are using async manner and
+//It will not wait for the call function to be executed first and then go to next line.
